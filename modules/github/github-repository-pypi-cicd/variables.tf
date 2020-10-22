f0956277beab7b99027f8393e6adaf70f191280c
Ryan Schreiber <ryan.schreiber@hp.com>
2020-10-21 18:42:14 -0700

    adding variable validations

--- modules/github/github-repository-pypi-cicd/variables.tf
+++ modules/github/github-repository-pypi-cicd/variables.tf
@@ -1,16 +1,33 @@
 
 variable "git_token" {
   type    = string
+  description = "The personal Git token for the owner of this repository."
+  validation {
+    condition     = length(var.git_token) == 20
+    error_message = "Please provide a valid git token, length 20 characters."
+  }
 }
 
 variable "project_name" {
-  type    = string
+  type        = string
+  description = "The name of your project, restricted to only [A-Za-z0-9_]"
+  validation {
+    condition     = length(var.project_name) > 4 && can(regex("^[A-Za-z0-9_]$", var.project_name))
+    error_message = "The project_name value cannot be an empty string, and restricted to only [A-Za-z0-9_] characters"
+  }
 }
 
 variable "project_description" {
-  type    = string
+  type        = string
+  description = "An optional, short description for your package."
+  default     = " "
 }
 
 variable "pypi_token" {
-  type    = string
+  type        = string
+  description = "A PYPI token for the account you want to publish your package to."
+  validation {
+    condition     = length(var.pypi_token) == 172 && substr(var.pypi_token, 0, 5) == "pypi-"
+    error_message = "The pypi_token value must be a valid PYPI token starting with \"pypi-\"."
+  }
 }
